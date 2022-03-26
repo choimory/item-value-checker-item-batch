28168261b2468ad803fdae75310ad651efe30b39
choimory <choimory@naver.com>
2022-03-26 19:11:47 +0900

    Initial commit

---
+++ src/main/java/com/choimory/itemvaluechecker/batch/itembatch/ItemValueCheckerItemBatchApplication.java
@@ -0,0 +1,13 @@
+package com.choimory.itemvaluechecker.batch.itembatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemValueCheckerItemBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemValueCheckerItemBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/choimory/itemvaluechecker/batch/itembatch/ItemValueCheckerItemBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.itemvaluechecker.batch.itembatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemValueCheckerItemBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
